de5cc99c5ac1e22a405793b5440d99c0e11c7569
moznion <moznion@gmail.com>
2013-11-10 01:30:54 +0900

    Implement basic functions.

--- app.pl
+++ app.pl
@@ -2,21 +2,54 @@
 
 use strict;
 use warnings;
+use utf8;
+use FindBin;
+use Getopt::Long;
+use JSON;
 use Plack::Request;
+use Yancha::Bot2;
 
 sub response {
     my ($bot, $json) = @_;
-}
 
-my $bot;
+    my $repos = $json->{repository};
+
+    my $message = sprintf(
+        "[%s] %s (%s) %s",
+        $json->{status_message},
+        $repos->{name},
+        $json->{commit},
+        $json->{build_url}
+    );
+
+    $bot->post($message);
+    print "$message\n";
+}
 
 my $app = sub {
     my $req = Plack::Request->new(shift);
 
-    if ($req->method eq 'POST') {
-        my $json = $req->param;
-        use Data::Dumper::Concise; warn Dumper($json); # TODO remove
+    if ($req->method eq 'POST' and my $payload = $req->param('payload')) {
+        my $json = decode_json($payload);
         response($bot, $json);
         return [200, [], ['']];
     }
+
+    warn "[ERROR] $req\n";
+    return [500, [], ['']];
 };
+
+# Get configurations.
+my $config = do("$FindBin::Bin/config.pl");
+
+# Setting for host and port.
+GetOptions( \my %option, qw/host=s port=i/, );
+my $server_conf = $config->{Server};
+$option{host} ||= $server_conf->{host};
+$option{port} ||= $server_conf->{port};
+unless ( $option{host} && $option{port} ) {
+    die '! Please specify host and port in config.pl';
+}
+
+my $bot = Yancha::Bot2->new($config);
+$bot->up($app);
